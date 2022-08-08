4bc37fa41035a1d6ce52e70b57ca75ddd34a2bc1
Louise <louisegabriellecc@gmail.com>
2022-08-08 15:55:24 +0100

    Correction - Typo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import {NavBar, Footer, Sidebar, ThemeSettings } from "./components"
+import {Navbar, Footer, Sidebar, ThemeSettings } from "./components"
 import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramind, Customers, Kanban, Area, Line, Bar, Pyramid, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages'
 import "./App.css";
 
@@ -41,7 +41,7 @@ const App = () => {
                     ${activeMenu ? "md:ml-72" : "flex-2"}`}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-              <NavBar/> 
+              <Navbar/> 
               </div>
           </div>
           <div>
