fdc3a0ddf9c60dfb8089c23a214c5470b49fe1a2
Louise <louisegabriellecc@gmail.com>
2022-08-08 16:28:37 +0100

    Sidebar - close button implemented

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -10,12 +10,13 @@ const initialState = {
 };
 
 export const ContextProvider = ({ children }) => {
-  const [activeMenu, setactiveMenu] = useState(true);
+  const [activeMenu, setActiveMenu] = useState(true);
 
   return (
     <StateContext.Provider
       value={{
         activeMenu,
+        setActiveMenu,
       }}
     >
       {children}
