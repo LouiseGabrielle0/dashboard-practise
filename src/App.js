1d424ed29458fa75ca2fc53b84a6f9f4948bf498
Louise <louisegabriellecc@gmail.com>
2022-08-09 14:09:35 +0100

    Correction - Div in the wrong place + formatting

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import {Navbar, Footer, Sidebar, ThemeSettings } from "./components"
-import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramind, Customers, Kanban, Area, Line, Bar, Pyramid, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages'
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramind,
+  Customers,
+  Kanban,
+  Area,
+  Line,
+  Bar,
+  Pyramid,
+  Pie,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Editor,
+} from "./pages";
 import "./App.css";
-import {useStateContext} from './contexts/ContextProvider'
+import { useStateContext } from "./contexts/ContextProvider";
 
 const App = () => {
-  const {activeMenu} = useStateContext()
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
@@ -42,35 +60,36 @@ const App = () => {
                     ${activeMenu ? "md:ml-72" : "flex-2"}`}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-              <Navbar/> 
-              </div>
-          </div>
-          <div>
-            <Routes>
-              {/* Dashboard */}
-              <Route path="/" element={<Ecommerce/>} />
-              <Route path="/ecommerce" element={<Ecommerce/>} />
-              {/* Pages */}
-              <Route path="/orders" element={<Orders/>} />
-              <Route path="/employees" element={<Employees/>} />
-              <Route path="/customers" element={<Customers/>} />
+              <Navbar />
+            </div>
 
-              {/* Apps */}
-              <Route path="/kanban" element={<Kanban/>} />
-              <Route path="/editor" element={<Editor/>} />
-              <Route path="/calendar" element={<Calendar/>} />
-              <Route path="/color-picker" element={<ColorPicker/>} />
+            <div>
+              <Routes>
+                {/* Dashboard */}
+                <Route path="/" element={<Ecommerce />} />
+                <Route path="/ecommerce" element={<Ecommerce />} />
+                {/* Pages */}
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} />
 
-              {/* Charts */}
-              <Route path="/line" element={<Line/>} />
-              <Route path="/area" element={<Area/>} />
-              <Route path="/bar" element={<Bar/>} />
-              <Route path="/pie" element={<Pie/>} />
-              <Route path="/financial" element={<Financial/>} />
-              <Route path="/color-mapping" element={<ColorMapping/>} />
-              <Route path="/pyramid" element={<Pyramid/>} />
-              <Route path="/stacked" element={<Stacked/>} />
-            </Routes>
+                {/* Apps */}
+                <Route path="/kanban" element={<Kanban />} />
+                <Route path="/editor" element={<Editor />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/color-picker" element={<ColorPicker />} />
+
+                {/* Charts */}
+                <Route path="/line" element={<Line />} />
+                <Route path="/area" element={<Area />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/financial" element={<Financial />} />
+                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/pyramid" element={<Pyramid />} />
+                <Route path="/stacked" element={<Stacked />} />
+              </Routes>
+            </div>
           </div>
         </div>
       </BrowserRouter>
@@ -79,5 +98,3 @@ const App = () => {
 };
 
 export default App;
-
-
